dcfae11db40667731088ee669bdc064a28ef5c35
kshitijslays <rajkshitij876@gmail.com>
2025-07-27 08:38:04 +0530

    started dsa with basic question of TwoNum

---
+++ TwoNums.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end())
+            {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+
+    vector<int> result=s.twoSum(nums,target);
+    for(int index: result){
+        cout<<index<<" ";
+    }
+    return 0;
+}
